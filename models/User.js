88ecfb2b3ae9d387081c26fa5de6a549b67be1f5
ashutoshrai204 <ashutoshrai204@gmail.com>
2024-08-24 04:40:28 +0530

    initial commit

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user", // default to lowercase "user"
+      enum: ["user", "admin"], // enforce allowed values
+      lowercase: true, // always store in lowercase
+    },
+    mainrole: {
+      type: String,
+      default: "user",
+    },
+    subscription: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Courses",
+      },
+    ],
+    resetPasswordExpire: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", schema);
